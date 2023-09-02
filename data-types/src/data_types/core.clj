d0650a1aecc3f34a84dde2a63fe07eac68c544ec
Heiner Nuñez <heiner23.0@gmail.com>
2023-09-02 17:14:48 -0500

    feat: encode text exercise

--- data-types/src/data_types/core.clj
+++ data-types/src/data_types/core.clj
@@ -1,6 +1,30 @@
-(ns data-types.core)
+(ns data-types.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn encode-letter
+  [s x]
+  (let [code (Math/pow (+ x (int (first (char-array s)))) 2)]
+    (str "#" (int code))))
+
+(defn encode
+  [s]
+  (let [number-of-words (count (str/split s #" "))]
+    (str/replace s #"\w" (fn [s] (encode-letter s number-of-words)))))
+
+(defn decode-letter
+  [x y]
+  (let [number (Integer/parseInt (subs x 1))
+        letter (char (- (Math/sqrt number) y))]
+    (str letter)))
+
+(defn decode
+  [s]
+  (let [number-of-words (count (str/split s #" "))]
+    (str/replace s #"\#\d+" (fn [s] (decode-letter s number-of-words)))))
+
+(defn main []
+  (println (encode-letter "a" 2))
+  (println (encode "Hello World"))
+  (println (encode "Super secret"))
+  (println (encode "Super secret message"))
+  (println (decode (encode "If you want to keep a secret, you must also hide it from yourself"))))
